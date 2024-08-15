0c38319600bc6a67e994c15821047962a94c838e
PlayQuartz <57173794+PlayQuartz@users.noreply.github.com>
2024-08-15 12:07:04 +0200

    Fix HashRouter problems

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from "react"
-import { HashRouter as Router, Route, Switch  } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes  } from 'react-router-dom';
 import LeaderboardWolt from './LeaderboardWolt';
 import LeaderboardReddyshRoyale from './LeaderboardReddyshRoyale'
 import TwitchPolls from './TwitchPolls';
@@ -9,11 +9,11 @@ import TwitchPolls from './TwitchPolls';
 function App() {
     return (
       <Router>
-        <Switch >
+        <Routes >
           <Route path="/wolt_leaderboard" element={<LeaderboardWolt />} />
           <Route path="/reddysh_royale_leaderboard" element={<LeaderboardReddyshRoyale />} />
           <Route path="/twitch_polls" element={<TwitchPolls />} />
-        </Switch >
+        </Routes >
       </Router>
     );
   }
